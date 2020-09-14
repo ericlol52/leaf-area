bb41ee71aa8ce76a7727ee1a5b0d19dbf3a28819
juniorbassani <junior.eduardo.bassani@gmail.com>
2020-09-14 12:49:48 -0300

    new video-proc subdirectory

--- leaf_area.cpp
+++ leaf_area.cpp
@@ -18,74 +18,74 @@ const int _ratio = 3;
 const int kernel_size = 3;
 
 void show_wait_destroy(const char *winname, const Mat& img);
-void CannyThreshold(int, void *);
+void canny_threshold(int, void *);
 void thresh_cb(int, void *);
 
 void help()
 {
-	cout << "Usage: ./leaf-area [image.jpg]" << endl;
+    cout << "Usage: ./leaf-area [image.[jpg|png]]" << endl;
 }
 
 int main(int argc, char **argv)
 {
-	if (argc < 2) {
-		help();
-		return -1;
-	}
+    if (argc < 2) {
+        help();
+        return -1;
+    }
 
-	src = imread(argv[1], IMREAD_COLOR);
+    src = imread(argv[1], IMREAD_COLOR);
 
-	if (src.empty()) {
-		cout << "The image" << argv[1] << " could not be loaded." << endl;
-		return -1;
-	}
+    if (src.empty()) {
+        cout << "The image" << argv[1] << " could not be loaded." << endl;
+        return -1;
+    }
 
-	cvtColor( src, gray_img, COLOR_BGR2GRAY );
-	namedWindow( window_name, WINDOW_AUTOSIZE );
-	createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
-	CannyThreshold(0, 0);
-	waitKey();
+    cvtColor(src, gray_img, COLOR_BGR2GRAY);
+    namedWindow(window_name, WINDOW_AUTOSIZE);
+    createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, canny_threshold);
+    canny_threshold(0, nullptr);
+    waitKey();
 
-	return 0;
+    return 0;
 }
 
-void CannyThreshold(int, void*)
+void canny_threshold(int, void *)
 {
-	/*
-	 * Blurring the image might reduce or eliminate noise, which can
-	 * be inconvenient if we're trying to measure leaf nerves.
-	 * The resulting blur level is proportional to the Size() of the kernel;
-	 * adjust if necessary.
-	 */
-	blur( gray_img, edges, Size(1,1) );
-	Canny( edges, edges, lowThreshold, lowThreshold*_ratio, kernel_size );
-	dst = Scalar::all(0);
-	src.copyTo( dst, edges);
-	imshow( window_name, dst );
+    /*
+    * Blurring the image might reduce or eliminate noise, which can
+    * be inconvenient if we're trying to measure leaf nerves.
+    * The resulting blur level is proportional to the Size() of the kernel;
+    * adjust if necessary.
+    */
+    blur(gray_img, edges, Size(1,1));
+    Canny(edges, edges, lowThreshold, lowThreshold*_ratio, kernel_size);
+    dst = Scalar::all(0);
+    src.copyTo(dst, edges);
+    imshow(window_name, dst);
 }
 
 void thresh_cb(int, void *)
 {
-	Mat canny_output;
-	Canny(gray_img, canny_output, thresh, thresh * 2);
+    Mat canny_output;
+    Canny(gray_img, canny_output, thresh, thresh * 2);
 
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-	findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
-	vector<vector<Point>> hull(contours.size());
-	for (size_t i = 0; i < contours.size(); i++)
-		convexHull(contours[i], hull[i]);
+    vector<vector<Point>> hull(contours.size());
+    for (size_t i = 0; i < contours.size(); i++)
+        convexHull(contours[i], hull[i]);
 
-	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+    Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
 
-	for (size_t i = 0; i < contours.size(); i++) {
-		Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256));
-		drawContours(drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
-		drawContours(drawing, hull, (int)i, color);
-	}
+    for (size_t i = 0; i < contours.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256));
+        drawContours(drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
+        drawContours(drawing, hull, (int)i, color);
+    }
 
-	imshow("Contours", drawing);
+    imshow("Contours", drawing);
 }
 
 /**
@@ -96,8 +96,8 @@ void thresh_cb(int, void *)
  */
 void show_wait_destroy(const char *winname, const Mat& img)
 {
-	imshow(winname, img);
-	moveWindow(winname, 500, 0);
-	waitKey();
-	destroyWindow(winname);
+    imshow(winname, img);
+    moveWindow(winname, 500, 0);
+    waitKey();
+    destroyWindow(winname);
 }
